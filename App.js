224c300193ea1d24c20b51a94bb9bc77c7ba45a1
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-05-30 08:47:42 -0500

    feat: First state added

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
+import { themes } from './src/constants/themes';
 
 export default function App() {
+  const [task, setTask] = useState("");
+
+  const handleChangeText = (value) => {
+    setTask(value);
+    console.log(task);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={themes.container}>
+      <View style={styles.containerTask}>
+        <TextInput
+          style={styles.inputTask}
+          placeholder='Add new task'
+          value={task} 
+          onChangeText={text => handleChangeText(text)} />
+        <Button title='ADD' color='#8CBCB9'/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerTask: {
+    padding: 25,
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  inputTask: {
+    borderColor: '#8CBCB9',
+    borderBottomWidth: 1,
+    marginBottom: 10,
+    width: '60%',
+    height: 40,
+    fontSize: 14,
+    color: '#212121',
+  }
 });
+
---
+++ src/constants/themes/index.js
@@ -0,0 +1,9 @@
+export const themes = {
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: '#000000',
+    }
+}
